781440facb3abbc1f10ef8500edb8cb3ec535448
Laboratorios <Laboratorios@E11>
2018-11-17 10:36:33 -0600

    ORM inicio

---
+++ Progra4.AW.Products/Progra4.AW.Products.Model/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra4.AW.Products.Model
+{
+    class Class2
+    {
+    }
+}
---
+++ Progra4.AW.Products/Progra4.AW.Products.Model/MisTablas/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra4.AW.Products.Model
+{
+    public partial class Product
+    {
+        public float SizeInInches { get {
+                float elResultado = 0;
+                if (SizeUnitMeasureCode != null && SizeUnitMeasureCode == "CM")
+                {
+                    
+                    float tamanoFloat = 0;
+                    float.TryParse(Size, out tamanoFloat);
+                    elResultado = tamanoFloat / (float)2.54;
+                   
+                    
+
+                }
+                return elResultado;
+            } set { }
+        }
+
+    }
+}
